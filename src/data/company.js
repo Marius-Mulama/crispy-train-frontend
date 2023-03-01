0225384c9d773ccdf83d044abe02ea96e3bfd323
Marius <mulamamarius@gmail.com>
2023-03-01 23:35:28 +0300

    Add dummy data which will be used later

---
+++ src/data/company.js
@@ -0,0 +1,11 @@
+export const profileData = 
+  {
+    id:1,
+    fullname: "John Doe",
+    gender: "Male",
+    contact: "Now",
+    location:"Nairobi",
+    email:"user@email.com",
+    summary:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+    github:""
+  };
---
+++ src/data/experience-data.js
@@ -0,0 +1,42 @@
+export const experienceData = [
+  {
+    id:1,
+    company: "Company A",
+    from: "March 2022",
+    to: "Now",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+  },
+  {
+    id:2,
+    company: "Company A",
+    from: "March 2021",
+    to: "March 2022",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+  },
+  {
+    id:3,
+    company: "Company B",
+    from: "March 2021",
+    to: "March 2020",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+  },
+  {
+    id:4,
+    company: "Company C",
+    from: "March 2020",
+    to: "March 2019",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+  },
+  {
+    id:5,
+    company: "Company D",
+    from: "March 2019",
+    to: "March 2018",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut dolor id orci venenatis gravida ullamcorper vel tortor. Nullavestibulum gravida urna, eu eleifend massa consequat sit amet.Integer et eros blandit, luctus tellus sit amet, fringilla urna",
+  }
+];
