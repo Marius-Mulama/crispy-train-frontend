256a2a0418cc3e024f68755f5739ecf00ead3a37
Marius <mulamamarius@gmail.com>
2023-03-03 08:29:26 +0300

    some changes before adding networking

--- src/App.js
+++ src/App.js
@@ -12,45 +12,49 @@ import Profile from './pages/Profile';
 import { useEffect, useState } from 'react';
 // import User from './components/User';
 import AccountCard from './components/AccountCard';
+import AddExperience from './components/AddExperience';
 
 function App() {
 
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  useEffect(()=>{
-    const getUser = ()=>{
-      fetch("http://localhost:8000/auth/login/success",{
-        method:"GET",
-        credentials:"include",
-        headers:{
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        }
-      }).then(response=>{
-        console.log(1)
-        if(response.status === 200){
-          return response.json()
-        }else{
-          throw new Error("Authentication has failed")
-        }
-      }).then((resObject)=>{
-        console.log(2)
-        setUser(resObject.user)
-      }).catch((err)=>{
-        console.log(err)
-      })
-    }
+  // useEffect(()=>{
+  //   const getUser = ()=>{
+  //     fetch("http://localhost:8000/auth/login/success",{
+  //       method:"GET",
+  //       credentials:"include",
+  //       headers:{
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json",
+  //         "Access-Control-Allow-Credentials": true,
+  //       }
+  //     }).then(response=>{
+  //       console.log(1)
+  //       if(response.status === 200){
+  //         return response.json()
+  //       }else{
+  //         throw new Error("Authentication has failed")
+  //       }
+  //     }).then((resObject)=>{
+  //       console.log(2)
+  //       setUser(resObject.user)
+  //     }).catch((err)=>{
+  //       console.log(err)
+  //     })
+  //   }
 
-    getUser()
-  },[])
+  //   getUser()
+  // },[])
+
+  // setUser(true)
+  const user = true;
 
   console.log(user)
 
   return (
     <Router>
       <Header user={user}/>
-      <div className='bg-gray-800  dark:text-gray-100 h-full' >
+      <div className='bg-gray-800  dark:text-gray-100 min-h-screen' >
         <Routes>
           <Route path={"/"} element={user? <Home/> : <Welcome/>} />
           <Route path={"/login"} element={user ? <Navigate to="/" /> : <Authentication bool={true} />}/>
@@ -58,6 +62,7 @@ function App() {
           <Route path={"/profile"} element={<Profile user={user}/>} />
           <Route path={"/user"} element={<AccountCard/>} />
           <Route path={"/500"} element={<ServerError/>} />
+          <Route path={"/test"} element={<AddExperience/>} />
           <Route path={"*"} element={<NotFound/>} />
         </Routes>
       </div>
